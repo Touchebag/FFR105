79fb5d754caf634b8016902891682b399ca0216a
Touchebag <someone90_6@hotmail.com>
2014-09-10 18:10:37 +0200

    Renamed file

--- programming_assignment/PolynomialDifferantation.m
+++
@@ -1,7 +0,0 @@
-% Returns the specified order derviative of a polynom
-function derviative = PolynomialDifferentiation(inVector, diffOrder)
-
-  % Matlab still uses descending order
-  polynomial = fliplr(inVector);
-
-  diff(polynomial, diffOrder);
