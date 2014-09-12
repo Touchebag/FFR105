63a42ece0bbc9560f6a9cec5fb6da30ba873ecda
Touchebag <someone90_6@hotmail.com>
2014-09-12 16:06:04 +0200

    Can now plot stuff

---
+++ programming_assignment/PlotIterations.m
@@ -0,0 +1,16 @@
+% Takes a polynomial and a vector of iteratino values and plots them
+function Plotiterations(polynomial, values)
+
+  hold on;
+
+  poly = fliplr(polynomial);
+  % 100 data points accuracy for plotting
+  points = linspace(min(values) - 2, max(values) + 2, 100);
+
+  % Plot function
+  plot(points, Polynomial(points, poly));
+  % Plot NR iteration values
+  scatter(values, Polynomial(values, poly));
+
+  hold off;
+
