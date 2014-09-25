c124cde9140ad191e8876e9ca3c5af3088812018
Touchebag <someone90_6@hotmail.com>
2014-09-25 12:15:44 +0200

    Really fixed tournament selection now

--- homework_1/1.3/TournamentSelect.m
+++ homework_1/1.3/TournamentSelect.m
@@ -4,30 +4,32 @@ function index = TournamentSelection(population, selectionParamater, size)
   popSize         = length(population);
   tempPopulation  = population;
   selected        = [];
-  originalIndexes = [];
 
   % Randomly select individuals
   for i = 1:size
     r = randi([1 popSize]);
 
-    % Save index
-    originalIndexes = [originalIndexes r];
-    selected        = [selected population(r)];
+    individual = population(r:end);
+    fitness = EvaluateIndividual(individual);
+
+    % Save induvidual and index
+    temp     = [fitness r];
+    selected = [selected; temp];
   end
 
-  % Sort fitness values but keep track of positions
-  [sortedFitness sortedIndexes] = sort(selected, 'descend')
-
-  % Select worst individual as default
-  temp  = sortedIndexes(size);
-  index = originalIndexes(temp);
+  % Sort selected individuals
+  sortedFitness = sortrows(selected);
+  sortedFitness = flipud(sortedFitness);
 
   for i = 1:size
     r = rand;
 
+    % This is outside of the if statement to ensure that
+    % we always return a valid index (defaults to the worst)
+    index = sortedFitness(i, 2);
+
     if r < selectionParamater
-      temp  = originalIndexes(i);
-      index = originalIndexes(temp);
+      break;
     end
   end
 
