3d0e0f5b9244f2332106d156bef3de00633b2e59
Touchebag <someone90_6@hotmail.com>
2014-10-06 14:09:38 +0200

    GA21b works

--- homework_2/2.1/GA21b.m
+++ homework_2/2.1/GA21b.m
@@ -1,27 +1,32 @@
 % Searches for the minimum of a function using a genetic algorithm
 
+  addpath("./TSPgraphics");
+  tspFigure = InitializeTspPlot(cityLocation,[0 20 0 20]);
+  connection = InitializeConnections(cityLocation);
+  PlotPath(connection,cityLocation,path);
+
   populationSize                = 100;
   numberOfGenes                 = 50;
   mutationProbability           = 0.025;
   tournamentSize                = 2;
   tournamentSelectionParamenter = 0.75;
   numberOfGenerations           = 100;
-  fitness                       = zeros(populationSize, 1);
+  fitness                       = zeros(1,populationSize);
 
   population = InitializePopulation(populationSize, numberOfGenes);
+  cityMatrix = LoadCityLocations;
 
   for iGeneration = 1:numberOfGenerations
 
-    maximumFitness = 0.0;
+    bestFitness         = intmax;
     bestIndividualIndex = 0;
 
     for i = 1:populationSize
       chromosome = population(i,:);
-      fitness(i) = EvaluateIndividual(x);
-      decodedPopulation(i,:) = x;
+      fitness(i) = EvaluatePath(chromosome, cityMatrix);
 
-      if fitness(i) > maximumFitness
-        maximumFitness = fitness(i);
+      if fitness(i) < bestFitness
+        bestFitness = fitness(i);
         bestIndividualIndex = i;
       end
     end
@@ -31,7 +36,7 @@
     for i = 1:populationSize
       i1 = TournamentSelect(fitness, tournamentSelectionParamenter, tournamentSize);
 
-      tempPopulation(i)     = population(i1,:);
+      tempPopulation(i,:) = population(i1,:);
     end
 
     for i = 1:populationSize
@@ -43,6 +48,8 @@
     bestIndividual = population(bestIndividualIndex,:);
     tempPopulation = InsertBestIndividual(tempPopulation, bestIndividual, 1);
     population     = tempPopulation;
+
+    PlotPath(connection,cityLocation,bestIndividual);
 
   end
 
--- homework_2/2.1/Mutate.m
+++ homework_2/2.1/Mutate.m
@@ -7,6 +7,7 @@ function newChromosome = Mutate(chromosome, mutationProbability)
   for i = 1:chromLength
     r = rand;
 
+    %TODO allow swapping with iteslf?
     if r < mutationProbability
       r = randi([1 chromLength]);
       newChromosome(i) = chromosome(r);
