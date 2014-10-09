5500d26d962119d0f67092ad9f1bfa64ea8c1d34
Touchebag <someone90_6@hotmail.com>
2014-10-09 13:57:59 +0200

    Added files for problem c

---
+++ homework_2/2.1/c/GetNearestNeighbourPathLength.m
@@ -0,0 +1,41 @@
+function pathLength = GetNearestNeighbourPathLength(cityLocation)
+
+  global distanceMatrix;
+
+  % Create matrix
+  numberOfCities       = size(cityLocation, 1)
+  unvisited            = [1:numberOfCities];
+  nearestNeighbourPath = zeros(1, numberOfCities);
+
+  % Random start node
+  r                       = randi(numberOfCities);
+  nearestNeighbourPath(1) = r;
+  previousCity            = r;
+  unvisited(r)            = [];
+
+  for i = 1:(numberOfCities - 1)
+    citiesLeft = length(unvisited);
+    shortestDistance = intmax;
+
+    for j = 1:citiesLeft
+      currentCity = unvisited(j);
+
+      % Find nearest neighbour
+      if distanceMatrix(previousCity,currentCity) < shortestDistance
+        nearestNeighbourIndex = j;
+        nearestNeighbour = currentCity;
+        shortestDistance = distanceMatrix(previousCity, currentCity);
+      end
+    end
+
+    % Add nearest neighbour to path and remove from unvisisted
+    nearestNeighbourPath(i + 1)      = nearestNeighbour;
+    unvisited(nearestNeighbourIndex) = [];
+    previousCity                     = nearestNeighbour;
+
+  end
+
+  pathLength = GetPathLength(nearestNeighbourPath, cityLocation)
+
+end
+
---
+++ homework_2/2.1/c/GetPathLength.m
@@ -0,0 +1,27 @@
+% Calculates the length of a path
+function pathLength = EvaluatePath(path, cityMatrix)
+
+  global distanceMatrix;
+
+  pathLength = 0.0;
+  numberOfCities = length(path);
+
+  for i = 1:(numberOfCities - 1)
+    city1 = path(i);
+    city2 = path(i + 1);
+
+    dist = distanceMatrix(city1, city2);
+
+    pathLength = pathLength + dist;
+  end
+
+  % For the return path from the last city
+  city1 = path(numberOfCities);
+  city2 = path(1);
+
+  dist = distanceMatrix(city1, city2);
+
+  pathLength = pathLength + dist
+
+end
+
