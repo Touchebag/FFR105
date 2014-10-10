bef06875bc802a7851b10ceb4420d58488d744c2
Touchebag <someone90_6@hotmail.com>
2014-10-10 11:09:26 +0200

    Wrote InitializePheromoneLevels

--- homework_2/2.1/c/InitializePheromoneLevels.m
+++ homework_2/2.1/c/InitializePheromoneLevels.m
@@ -3,9 +3,6 @@ function pheromoneLevels = InitializePheromoneLevels(numberOfCities, tau0);
 
   pheromoneLevels = zeros(numberOfCities);
 
-  % NOTE: For simplicity we calculate pheromoneLevels(i,j) and
-  % pheromoneLevels(j,i) separately. We just need to make sure that
-  % we set both when changing the values elsewhere.
   for i = 1:numberOfCities
     for j = 1:numberOfCities
       pheromoneLevels(i,j) = tau0;
