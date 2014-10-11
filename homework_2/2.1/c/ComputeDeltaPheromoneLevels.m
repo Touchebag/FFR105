f9047b35419a9d6c7196f69ee87887f680502c55
Touchebag <someone90_6@hotmail.com>
2014-10-11 20:14:03 +0200

    Fixed stupid

--- homework_2/2.1/c/ComputeDeltaPheromoneLevels.m
+++ homework_2/2.1/c/ComputeDeltaPheromoneLevels.m
@@ -1,27 +1,27 @@
 % Calculates the amount of pheromones to be dispersed
 function pheromoneDeltaMatrix = ComputeDeltaPheromoneLevels(pathCollection, pathLengthCollection)
 
-  numberOfAnts         = length(pathCollection);
+  numberOfAnts         = size(pathCollection,1);
   numberOfCities       = size(pathCollection,2);
-  pheromoneDeltaMatrix = zeros(50 ,50);
+  pheromoneDeltaMatrix = zeros(numberOfCities);
 
   for i = 1:numberOfAnts
     pheromoneAmount = 1 / pathLengthCollection(i);
 
     for j = 1:numberOfCities - 1
-      city1 = pathCollection(j);
-      city2 = pathCollection(j + 1);
+      city1 = pathCollection(i, j);
+      city2 = pathCollection(i, j + 1);
 
-      pheromoneDeltaMatrix(j, j + 1) = ...
-        pheromoneDeltaMatrix(j, j + 1) + pheromoneAmount;
+      pheromoneDeltaMatrix(city1, city2) = ...
+        pheromoneDeltaMatrix(city1, city2) + pheromoneAmount;
     end
 
     % Return trip
-    city1 = pathCollection(numberOfCities);
-    city2 = pathCollection(1);
+    city1 = pathCollection(i, numberOfCities);
+    city2 = pathCollection(i, 1);
 
-    pheromoneDeltaMatrix(numberOfCities, 1) = ...
-      pheromoneDeltaMatrix(numberOfCities, 1) + pheromoneAmount;
+    pheromoneDeltaMatrix(city1, city2) = ...
+      pheromoneDeltaMatrix(city1, city2) + pheromoneAmount;
 
   end
 
