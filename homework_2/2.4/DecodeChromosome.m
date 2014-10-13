f9326b79c4f2c337dfac68b91b3635f23976507f
Touchebag <someone90_6@hotmail.com>
2014-10-13 22:11:20 +0200

    Can nov execute code in chromosome

---
+++ homework_2/2.4/DecodeChromosome.m
@@ -0,0 +1,40 @@
+function values = ...
+  DecodeChromosome(chromosome, numberOfVRegisters, constantRegisters, inputs)
+
+  values               = zeros(1,length(inputs));
+  chrom                = chromosome.Chromosome;
+  numberOfInstructions = length(chrom) / 4;
+
+  for i = 1:length(inputs)
+
+    registers    = zeros(1,numberOfVRegisters);
+    registers    = [registers constantRegisters];
+    registers(1) = inputs(i);
+
+    for j = 1:numberOfInstructions
+      index = (j - 1) * 4 + 1;
+
+      operation = chrom(index);
+      output    = chrom(index + 1);
+      operand1  = chrom(index + 2);
+      operand2  = chrom(index + 3);
+
+      switch operation
+        case 1 % addition
+          registers(output) = registers(operand1) + registers(operand2);
+        case 2 % subtraction
+          registers(output) = registers(operand1) - registers(operand2);
+        case 3 % multiplication
+          registers(output) = registers(operand1) * registers(operand2);
+        case 4 % division
+          registers(output) = registers(operand1) / registers(operand2);
+      end
+
+    end
+
+    values(i) = registers(1);
+
+  end
+
+end
+
