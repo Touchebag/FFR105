8862fe4abd85dc63e3c246987f5059b154d2b536
Touchebag <someone90_6@hotmail.com>
2014-10-13 22:33:14 +0200

    Evaluation works

---
+++ homework_2/2.4/EvaluateIndividual.m
@@ -0,0 +1,10 @@
+% Calculates the fitness given output from a chromosome and reference values
+function fitness = EvaluateIndividual(values, inputs)
+
+  tmp = (values - inputs) .^ 2;
+  tmp = sum(tmp);
+  e   = sqrt(tmp / (length(values)));
+
+  fitness = 1 / e;
+end
+
