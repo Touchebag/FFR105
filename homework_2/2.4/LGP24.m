b614a7e07e5d9da03225d83d1cdf7c633359932b
Touchebag <someone90_6@hotmail.com>
2014-10-14 14:35:06 +0200

    Now works (I tihnk)

--- homework_2/2.4/LGP24.m
+++ homework_2/2.4/LGP24.m
@@ -1,31 +1,42 @@
 % Searches for the minimum of a function using a genetic algorithm
 
-  populationSize                = 100;
+  populationSize                = 50;
   crossoverProbability          = 0.8;
-  mutationProbability           = 0.025;
+  mutationProbability           = 0.25;
   tournamentSize                = 2;
   tournamentSelectionParamenter = 0.75;
-  numberOfGenerations           = 100;
+  numberOfGenerations           = 20;
+  minChromLength                = 2;
+  maxChromLength                = 10;
   fitness                       = zeros(populationSize, 1);
-  numberOfVariableRegisters     = 0;
-  constantRegisters             = [];
+  numberOfVariableRegisters     = 1;
+  constantRegisters             = [1 2 3 5 7 11];
   numberOfConstantRegisters     = length(constantRegisters);
 
-  population = InitializePopulation(populationSize, numberOfVariableRegisters);
+  inputs = LoadFunctionData;
+
+  population = ...
+    InitializePopulation(populationSize, ...
+                         minChromLength,            maxChromLength, ...
+                         numberOfVariableRegisters, numberOfConstantRegisters);
 
   for iGeneration = 1:numberOfGenerations
 
     maximumFitness = 0.0;
     bestIndividualIndex = 0;
+    decodedPopulation = zeros(populationSize, length(inputs));
 
     for i = 1:populationSize
-      chromosome = population(i,:);
-      x = DecodeChromosome(chromosome, 2, variableRange);
-      fitness(i) = EvaluateIndividual(x);
+      chromosome = population(i);
+      x = DecodeChromosome(chromosome, ...
+                           numberOfVariableRegisters, constantRegisters, ...
+                           inputs);
+      fitness(i) = EvaluateIndividual(x, inputs);
       decodedPopulation(i,:) = x;
 
       if fitness(i) > maximumFitness
         maximumFitness = fitness(i);
+        bestValues = x;
         bestIndividualIndex = i;
       end
     end
@@ -35,33 +46,44 @@
     for i = 1:2:populationSize
       i1 = TournamentSelect(fitness, tournamentSelectionParamenter, tournamentSize);
       i2 = TournamentSelect(fitness, tournamentSelectionParamenter, tournamentSize);
-      chromosome1 = population(i1,:);
-      chromosome2 = population(i2,:);
+      chromosome1 = population(i1);
+      chromosome2 = population(i2);
 
       r = rand;
       if r < crossoverProbability
         newChromosomePair     = Cross(chromosome1, chromosome2);
-        tempPopulation(i,:)   = newChromosomePair(1,:);
-        tempPopulation(i+1,:) = newChromosomePair(2,:);
+        tempPopulation(i)   = newChromosomePair(1);
+        tempPopulation(i+1) = newChromosomePair(2);
       else
-        tempPopulation(i,:)   = chromosome1;
-        tempPopulation(i+1,:) = chromosome2;
+        tempPopulation(i)   = chromosome1;
+        tempPopulation(i+1) = chromosome2;
       end
     end
 
     for i = 1:populationSize
-      originalChromosome = tempPopulation(i,:);
-      mutatedChromosome = Mutate(originalChromosome, mutationProbability);
-      tempPopulation(i,:) = mutatedChromosome;
+      originalChromosome = tempPopulation(i);
+      mutatedChromosome = Mutate(originalChromosome, mutationProbability, ...
+                                 numberOfVariableRegisters, ...
+                                 numberOfConstantRegisters);
+      tempPopulation(i) = mutatedChromosome;
     end
 
-    bestIndividual = population(bestIndividualIndex,:);
+    bestIndividual = population(bestIndividualIndex);
     tempPopulation = InsertBestIndividual(tempPopulation, bestIndividual, 1);
     population     = tempPopulation;
   end
 
-  disp('Minimum value');
-  disp(1 / maximumFitness);
-  disp('X1*, x2*');
-  disp(sprintf('%f, %f', bestIndividual(1), bestIndividual(2)));
+  xPlot = inputs(:,1);
+  y1Plot = inputs(:,2);
+  y2Plot = bestValues
+
+  plot(xPlot, y1Plot, xPlot, y2Plot)
+
+
+
+
+
+
+
+
 
