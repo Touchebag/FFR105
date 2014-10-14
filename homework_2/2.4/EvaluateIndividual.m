be81c31a6828d978e6c764bef9c5fa2751b2d852
Touchebag <someone90_6@hotmail.com>
2014-10-14 14:34:36 +0200

    Evaluate now uses correct values

--- homework_2/2.4/EvaluateIndividual.m
+++ homework_2/2.4/EvaluateIndividual.m
@@ -1,7 +1,7 @@
 % Calculates the fitness given output from a chromosome and reference values
 function fitness = EvaluateIndividual(values, inputs)
 
-  tmp = (values - inputs) .^ 2;
+  tmp = (values' - inputs(:,2)) .^ 2;
   tmp = sum(tmp);
   e   = sqrt(tmp / (length(values)));
 
